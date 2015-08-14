fde1cda88837bfe8a4d3e799e22a09e247d71dec
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-08-14 09:44:29 +0200

    startup script added, confirm-email-url changed

--- common/models/profile.js
+++ common/models/profile.js
@@ -3,7 +3,6 @@ import crypto from 'crypto';
 
 module.exports = function (Profile) {
 
-
   Profile.observe('before save', function (ctx, next) {
     if (ctx.isNewInstance) {
       // create verification token
@@ -18,18 +17,17 @@ module.exports = function (Profile) {
     }
   });
 
-
   Profile.observe('after save', function (ctx, next) {
     // was a new profile created?
     if (ctx.isNewInstance) {
-
       // create confirmation email content
       let Email = Profile.app.models.Email;
       let profileInstance = ctx.instance;
       let token = ctx.instance.verificationToken;
       let uid = ctx.instance.id;
-      let redirectUrl = 'http://www.google.com';
-      let confirmUrl = util.format('http://127.0.0.1:3000/api/Profiles/confirm?uid=%s&token=%s&redirect=%s', uid, token, redirectUrl);
+      let redirectUrl = '/login';
+      //let confirmUrl = util.format('http://127.0.0.1:3000/api/Profiles/confirm?uid=%s&token=%s&redirect=%s', uid, token, redirectUrl);
+      let confirmUrl = util.format('http://127.0.0.1:8080/confirm?uid=%s&token=%s&redirect=%s', uid, token, redirectUrl);
       let emailTemplate = '<p>Klik på linket for at bekræfte din nye brugerprofil:</p><a href=%s></a>';
 
       // send email
@@ -46,6 +44,5 @@ module.exports = function (Profile) {
     }
     next();
   });
-
 
 };
