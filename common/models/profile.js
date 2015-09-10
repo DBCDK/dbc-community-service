e2b368d6919772865e4052d5300f4869fab5f7c1
Martin Møller <mmj@dbc.dk>
2015-09-10 11:21:04 +0200

    basePath for redirect is sent as argument from application

--- common/models/profile.js
+++ common/models/profile.js
@@ -23,8 +23,9 @@ module.exports = function (Profile) {
     // was a new profile created?
     if (ctx.isNewInstance) {
       winston.info('created new profile');
+      winston.info('ctx.instance: ', ctx.instance);
       // create confirmation email content
-      let baseUrl = config.pgUrl;
+      let baseUrl = ctx.instance.basePath;
       let Email = Profile.app.models.Email;
       let profileInstance = ctx.instance;
       let token = ctx.instance.verificationToken;
