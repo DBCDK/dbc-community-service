9d2fba23706ce9700e15b4c1a2da18404af8ca93
Matias Rasmussen <rasmussen.matias@gmail.com>
2015-10-23 11:19:20 +0200

    Raise limit on json request size

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var boot = require('loopback-boot');
 var app = module.exports = loopback();
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.raw({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
 
 app.start = function() {
   // start the web server
