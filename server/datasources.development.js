eca4c5b7cbd5569726d7009289f394dfed8c095d
Jonas Peter Hyatt <jph@dbc.dk>
2016-01-19 09:52:34 +0100

    make dbc_config optional

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -4,6 +4,44 @@
  * @file Write a short description here.
  */
 
+import path from 'path';
 
-module.exports = require('@dbcdk/dbc-config').profileservice.development || null;
+let conf;
 
+if (process.env.USE_DEFAULT_CONFIG) { // eslint-disable-line
+  conf = {
+    db: {
+      name: 'db',
+      connector: 'memory'
+    },
+    psqlDs: {
+      host: '127.0.0.1',
+      port: 5432,
+      database: 'dbc_community',
+      username: 'communitydb',
+      password: 'loobbackcommunity',
+      name: 'psqlDs',
+      connector: 'postgresql'
+    },
+    emailDs: {
+      name: 'emailDs',
+      connector: 'mail',
+      transports: [
+        {
+          'type': 'smtp',
+          'host': 'mailhost.example.com',
+          'secure': false,
+          'port': 25,
+          'tls': {
+            'rejectUnauthorized': false
+          }
+        }
+      ]
+    }
+  };
+}
+else {
+  conf = require('@dbcdk/dbc-config').profileservice.development;
+}
+
+module.exports = conf;
