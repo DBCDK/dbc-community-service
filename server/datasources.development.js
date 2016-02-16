c017e0d5a1768794c5fd83a519be1ddfdbb1ccb0
Kasper Garnaes <kasper.garnaes@gmail.com>
2016-02-16 08:16:00 +0100

    Add support for environment configuration

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -38,6 +38,38 @@ if (process.env.USE_DEFAULT_CONFIG) { // eslint-disable-line
     }
   };
 }
+else if (process.env.USE_ENV_CONFIG) { // eslint-disable-line
+  conf = {
+    db: {
+      name: 'db',
+      connector: 'memory'
+    },
+    psqlDs: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_DB,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      name: 'psqlDs',
+      connector: 'postgresql'
+    },
+    emailDs: {
+      name: 'emailDs',
+      connector: 'mail',
+      transports: [
+        {
+          type: 'smtp',
+          host: 'mailhost.example.com',
+          secure: false,
+          port: 25,
+          tls: {
+            rejectUnauthorized: false
+          }
+        }
+      ]
+    }
+  };
+}
 else {
   conf = require('@dbcdk/biblo-config').communityservice.development;
 }
