fcc678fe208114eada003dff74310da8001ad246
Jonas Peter Hyatt <jph@dbc.dk>
2016-03-24 14:45:24 +0100

    sd-503: CheckIfUserIsQuarantined.

--- common/models/quarantine.js
+++ common/models/quarantine.js
@@ -1,5 +1,31 @@
 'use strict';
 
-module.exports = function(Quarantine) { // eslint-disable-line no-unused-vars
+module.exports = function(Quarantine) {
+  Quarantine.checkIfProfileIsQuarantined = function(ctx, id, next) {
+    Quarantine.find({
+      where: {
+        quarantinedProfileId: id,
+        end: {gte: (new Date())}
+      }
+    }, function (err, res) {
+      next(null, {
+        quarantined: (res.length > 0)
+      });
+    });
+  };
 
+  Quarantine.remoteMethod(
+    'checkIfProfileIsQuarantined',
+    {
+      description: 'Check if a profile is quarantined',
+      accepts: [
+        {arg: 'ctx', type: 'object', http: {source: 'context'}},
+        {arg: 'id', type: 'number', required: true}
+      ],
+      returns: {
+        arg: 'quarantine', type: 'object', root: true
+      },
+      http: {path: '/:id/check-if-profile-is-quarantined', verb: 'get'}
+    }
+  );
 };
