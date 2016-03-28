1d1c5e6c3d2f62659f557935ff90d4b8f4e03a32
Jonas Peter Hyatt <jph@dbc.dk>
2016-03-28 15:41:54 +0200

    sd-NaN: add testutils to create a profile, and login. Also test unilogin.

--- common/models/__tests__/profile.test.js
+++ common/models/__tests__/profile.test.js
@@ -5,6 +5,7 @@ process.env.TESTING = true;
 import expect from 'expect';
 import superagent from 'superagent';
 import app from '../../../server/server';
+import * as testutils from './testutils';
 
 describe('Test profile endpoints and functionality', () => {
   let server;
@@ -24,6 +25,21 @@ describe('Test profile endpoints and functionality', () => {
       .end((err, res) => {
         expect(err).toNotExist();
         expect(Array.isArray(res.body)).toExist();
+        done();
+      });
+  });
+  
+  it('should create a profile, and login via unilogin', (done) => {
+    let profile;
+
+    testutils.createProfile(app.get('url'))
+      .then((createResponse) => {
+        return testutils.loginViaUnilogin(app.get('url'), createResponse.username);
+      })
+      .then((loginResponse) => {
+        expect(typeof loginResponse.id).toEqual('string');
+        expect(typeof loginResponse.profileId).toEqual('number');
+        expect(typeof loginResponse.profile).toEqual('object');
         done();
       });
   });
