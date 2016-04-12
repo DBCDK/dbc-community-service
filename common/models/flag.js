f9dfde67d09b540d2453e5bd47f133bcbf9aa232
Matias Rasmussen <rasmussen.matias@gmail.com>
2016-04-12 10:39:55 +0200

    Added flags to review model

--- common/models/flag.js
+++ common/models/flag.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import {filter} from 'lodash';
+
 module.exports = function (Flag) { // eslint-disable-line no-unused-vars
 
   Flag.observe('before save', function afterFlagSave(ctx, next) {
@@ -17,11 +19,10 @@ module.exports = function (Flag) { // eslint-disable-line no-unused-vars
     const isPostFlag = typeof data.postFlagsId !== 'undefined' && data.postFlagsId;
     const isCommentFlag = typeof data.commentFlagsId !== 'undefined' && data.commentFlagsId;
     const isGroupFlag = typeof data.groupFlagsId !== 'undefined' && data.groupFlagsId;
+    const isReviewFlag = typeof data.reviewFlagsId !== 'undefined' && data.reviewFlagsId;
 
     if (
-      isCommentFlag && isPostFlag ||
-      isCommentFlag && isGroupFlag ||
-      isGroupFlag && isPostFlag
+      filter([isCommentFlag, isGroupFlag, isPostFlag, isReviewFlag]).length > 1
     ) {
       return next({error: 'Too many ids!'});
     }
@@ -74,6 +75,24 @@ module.exports = function (Flag) { // eslint-disable-line no-unused-vars
           }
           else {
             ctx.data._groups = instance;
+          }
+
+          next();
+        }
+      });
+    }
+    else if (isReviewFlag) {
+      Flag.app.models.review.findById(data.reviewFlagsId, (err, instance) => {
+        if (err) {
+          next(err);
+        }
+        else {
+
+          if (ctx.isNewInstance) {
+            ctx.instance.__data._reviews = instance;
+          }
+          else {
+            ctx.data._reviews = instance;
           }
 
           next();
