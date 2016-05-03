abbf96fd63c7432373eaeccb9923a537b5e33bdc
Jacob Andresen <jan@dbc.dk>
2016-05-03 09:48:49 +0200

    review - handle markedasdeleted

---
+++ server/migrations/psqlDs/20160502105032-add_markedas_deleted_to_review.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  up: function(dataSource, next) {
+    dataSource.connector.query('ALTER TABLE "public"."review" ADD COLUMN "markedasdeleted" BOOLEAN', next);
+  },
+  down: function(dataSource, next) {
+    dataSource.connector.query('ALTER TABLE "public"."review" DROP COLUMN "markedasdeleted" BOOLEAN', next);
+  }
+};
