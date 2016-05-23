c801613a6fa49ae898154ed76b57042304ba641e
Jonas Peter Hyatt <jph@dbc.dk>
2016-05-23 14:45:51 +0200

    sd-557: extend post and comment models to include a review

---
+++ server/migrations/psqlDs/20160523135247-attach_review_to_post_and_comment.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  up: function(dataSource, next) {
+    dataSource.connector.query('ALTER TABLE "public"."post" ADD COLUMN "attachedreviewid" INTEGER;', next);
+    dataSource.connector.query('ALTER TABLE "public"."comment" ADD COLUMN "attachedreviewid" INTEGER;', next);
+  },
+  down: function(dataSource, next) {
+    dataSource.connector.query('ALTER TABLE "public"."post" DROP COLUMN "attachedreviewid";', next);
+    dataSource.connector.query('ALTER TABLE "public"."comment" DROP COLUMN "attachedreviewid";', next);
+  }
+};
