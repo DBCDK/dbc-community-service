cc991a21be6da2ad478a6dd18d5e5ab7be850dc3
Jonas Peter Hyatt <jph@dbc.dk>
2016-07-27 10:12:22 +0200

    sd-630: Update relations between groups and profiles to allow additional properties in the through model.

---
+++ server/migrations/psqlDs/20160726125435-add-visited-to-group-profile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  up: function(dataSource, next) {
+    dataSource.connector.query('ALTER TABLE "public"."groupprofile" ADD COLUMN "visited" TIMESTAMP WITH TIME ZONE;', next);
+  },
+  down: function(dataSource, next) {
+    dataSource.connector.query('ALTER TABLE "public"."groupprofile" DROP COLUMN "visited";', next);
+  }
+};
