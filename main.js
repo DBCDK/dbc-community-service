be3dcb8e9427844310e43b2d521ebec1d8c76b14
Jonas Peter Hyatt <jph@dbc.dk>
2016-09-27 13:40:37 +0200

    use getDS in loopback-db-migrate

--- main.js
+++ main.js
@@ -2,11 +2,3 @@
 
 require('babel-core/register');
 require('./server/server');
-
-/*
-Promise.all(require('./server/ds').getDS()).then(() => {
-
-}).catch((err) => {
-  console.error(err);
-});
-*/
