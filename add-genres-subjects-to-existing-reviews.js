b8f760f94f0b67b2c55d4694fd33345eced73ced
Kasper Fuglsang <kfu@dbc.dk>
2017-07-12 11:30:44 +0200

    handle promise rejections

--- add-genres-subjects-to-existing-reviews.js
+++ add-genres-subjects-to-existing-reviews.js
@@ -25,28 +25,37 @@ superagent.get(`${csEndPoint}/api/reviews`).end(async (error, reviews) => {
     throw new Error('could not fetch reviews');
   }
   console.log(`Processing ${reviews.body.length} reviews`);
+  let numProcessed = 0;
 
   for(let i = 0; i < reviews.body.length; i++) {
-    const review = reviews.body[i];
-    const workResult = (await superagent
-      .get(`${opEndPoint}/v1/work`)
-      .query({pids: JSON.stringify([review.pid]), fields:['subjectDBCS', 'subjectGenre'], access_token: token})).body.data;
 
-    if (!workResult || workResult.length === 0) {
-      fs.appendFileSync(LOG_FILE, `Could not match work to review.id=${review.id}, review.pid=${review.pid}`);
-      console.log(`Could not match work to review.id=${review.id}, review.pid=${review.pid}`);
-    } else {
-      const work = workResult[0];
-      const subjects = work.subjectDBCS;
-      const genres = work.subjectGenre;
-      if (subjects && subjects.length > 0) {
-        await superagent.post(`${csEndPoint}/api/reviews/addSubject`)
-          .query({subject: subjects.join(','), reviewId: review.id});
+    const review = reviews.body[i];
+    try {
+      const workResult = (await superagent
+        .get(`${opEndPoint}/v1/work`)
+        .query({pids: JSON.stringify([review.pid]), fields:['subjectDBCS', 'subjectGenre'], access_token: token})).body.data;
+
+      if (!workResult || workResult.length === 0) {
+        fs.appendFileSync(LOG_FILE, `Could not match work to review.id=${review.id}, review.pid=${review.pid}`);
+        console.log(`Could not match work to review.id=${review.id}, review.pid=${review.pid}`);
+      } else {
+        const work = workResult[0];
+        const subjects = work.subjectDBCS;
+        const genres = work.subjectGenre;
+        if (subjects && subjects.length > 0) {
+          await superagent.post(`${csEndPoint}/api/reviews/addSubject`)
+            .query({subject: subjects.join(','), reviewId: review.id});
+        }
+        if (genres && genres.length > 0) {
+          await superagent.post(`${csEndPoint}/api/reviews/addGenre`)
+            .query({genre: genres.join(','), reviewId: review.id});
+        }
+        numProcessed++;
       }
-      if (genres && genres.length > 0) {
-        await superagent.post(`${csEndPoint}/api/reviews/addGenre`)
-          .query({genre: genres.join(','), reviewId: review.id});
-      }
+    } catch(err) {
+      fs.appendFileSync(LOG_FILE, `Some error occurred. review.id=${review.id}, review.pid=${review.pid}`);
+      console.log(`Some error occurred. review.id=${review.id}, review.pid=${review.pid}`, err);
     }
   }
+  console.log(`Processed ${numProcessed} reviews`);
 });
