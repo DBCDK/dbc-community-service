356e89ae1377dfdd2ea41ce3abe27aeadbf1350f
Søren Vibjerg <vibjerg@gmail.com>
2018-08-13 09:24:20 +0200

    Add tracker middlevare

---
+++ server/middlewares/tracker.js
@@ -0,0 +1,23 @@
+import {log} from 'dbc-node-logger';
+
+export default function tracker() {
+  return (req, res, next) => {
+    log.info('Request triggered', {
+      params: req.params,
+      query: req.query,
+      url: req.url
+    });
+    var start = process.hrtime();
+    res.once('finish', function() {
+      var diff = process.hrtime(start);
+      var timings = diff[0] * 1e3 + diff[1] * 1e-6;
+      log.info(`Request done`, {
+        timings,
+        params: req.params,
+        query: req.query,
+        url: req.url
+      });
+    });
+    next();
+  };
+}
